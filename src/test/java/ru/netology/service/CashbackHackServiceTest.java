45679f3edd19fa7353dcc35855f10c6651b723f2
Dima Melnikov <diman217@bk.ru>
2021-10-22 12:58:45 +0300

    testing

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,58 @@
-package ru.netology.service;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+class CashbackHackServiceTes {
+
+    CashbackHackService service;
+
+    {
+        service = new CashbackHackService();
+    }
+
+    @Test
+
+    public void shouldReturn500IfAmountIs500() {
+
+        int amount = 500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn100IfAmountIs1() {
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs2000() {
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    private void assertEquals(int actual, int expected) {
+    }
 }
