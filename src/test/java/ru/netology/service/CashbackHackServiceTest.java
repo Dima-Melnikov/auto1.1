e5f27ce9fe71f586d0cfb0beb1f588e51a8305db
Dima Melnikov <diman217@bk.ru>
2021-10-22 12:29:29 +0300

    tets

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,41 @@
-package ru.netology.service;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100IfAmountLowerThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnIfAmountGreaterThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
 }
